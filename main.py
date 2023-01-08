3f74d5e1c81123a103311628376b29df5f1a8f0d
Ritvik Buxi <66825562+Ritvik-Buxi@users.noreply.github.com>
2023-01-08 16:03:12 +0530

    Fix Main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from tkinter import *
-from tkinter import messagebox
 from tkinter import ttk
 from googletrans import Translator
 from googletrans import LANGUAGES
 from PIL import ImageTk, Image
+
 r = Tk()
 r.geometry('800x500')
 r.title("Translator")
 swap_img = ImageTk.PhotoImage(Image.open("swap.png"))
+
 logo = ImageTk.PhotoImage(Image.open("Google_Translate_logo.svg.png"))
 r.iconphoto(False,logo)
 r.config(bg="black")
-#            Head Label
+
 headlbl = Label(r,text="Language Translator",bg="black",fg="white",font=("Constantia",20,"bold"))
 headlbl.place(relx=0.5,rely=0.1,anchor=CENTER)
 #            Notify Label
@@ -35,7 +36,6 @@ DDoutlang.place(relx=0.75,rely=0.2,anchor=CENTER)
 #            Text Box Output
 textboxout = Text(r,bg="black",fg="white",height=10,width=20,padx=10,pady=1,bd=1,state="disabled")
 textboxout.place(relx=0.75,rely=0.5,anchor=CENTER)
-
 #            sWap Button
 lblcred = Label(r,text="Created by Ritvik Buxi 😎",bg="black",fg="white",font=("Stencil",15,"bold"))
 lblcred.place(relx=0.5,rely=0.9,anchor=CENTER)
@@ -48,15 +48,13 @@ def swap():
     DDinlang.set(ouou)
     DDoutlang.set(inin)
 def Translate():
-    translator = Translator(service_urls=[
-       'translate.google.com',
-       'translate.google.com.hk',
-       'translate.google.co.kr',
-     ])
-    t = textbox.get(1.0,"end")
-    translated = translator.translate(t,dest=DDoutlang.get(),src=DDinlang.get()).text
-    textboxout.delete(1.0,END)
-    textboxout.insert(END,translated)
+    translator = Translator()
+    try:
+        translated=translator.translate(text= textbox.get(1.0, END) , src = inin.get(), dest = ouou.get())
+        textboxout.delete(1.0, END)
+        textboxout.insert(END, translated.text)
+    except:
+        print("Try again")
 
 swap = Button(r,image=swap_img,command=swap)
 swap.place(relx=0.5,rely=0.2,anchor=CENTER)
